3dbb3e6cdd1bffccedf4ad1b08ddb0d1f2757519
dide <davi.alves@gft.com>
2022-01-05 16:57:12 -0300

    initial commit

---
+++ mister-barber-api/src/main/java/br/com/davi/mb/misterbarber/MisterBarberApplication.java
@@ -0,0 +1,13 @@
+package br.com.davi.mb.misterbarber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisterBarberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MisterBarberApplication.class, args);
+	}
+
+}
---
+++ mister-barber-api/src/test/java/br/com/davi/mb/misterbarber/MisterBarberApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.davi.mb.misterbarber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MisterBarberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
